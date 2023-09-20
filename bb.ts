72debae828be024cb274f7fc336112d51887c01d
Michael Schaffer <ichaelm1@gmail.com>
2023-09-20 15:49:25 -0400

    New motor blocks use inches and degrees

--- bb.ts
+++ bb.ts
@@ -4,18 +4,16 @@
 //% color=230 weight=50 icon="\uf067" block="Brainybot"
 namespace bb {
   const I2CADDR = 0x10;
-  const ADC0_REGISTER = 0x1e;
-  const ADC1_REGISTER = 0x20;
-  const ADC2_REGISTER = 0x22;
-  const ADC3_REGISTER = 0x24;
-  const ADC4_REGISTER = 0x26;
-  const LEFT_LED_REGISTER = 0x0b;
-  const RIGHT_LED_REGISTER = 0x0c;
-  const LEFT_MOTOR_REGISTER = 0x00;
-  const RIGHT_MOTOR_REGISTER = 0x02;
-  const LINE_STATE_REGISTER = 0x1d;
-  const VERSION_CNT_REGISTER = 0x32;
-  const VERSION_DATA_REGISTER = 0x33;
+  const MOTOR_SPEEDS_REGISTER = 0x00;
+
+  const FULL_SPEED_IPS = 8.5;
+  const FULL_SPEED_DPS = 240;
+
+  const SPEED_TO_IPS = FULL_SPEED_IPS / 100;
+  const SPEED_TO_DPS = FULL_SPEED_DPS / 100;
+
+  const SPEED_TO_IPMS = SPEED_TO_IPS / 1000;
+  const SPEED_TO_DPMS = SPEED_TO_DPS / 1000;
 
   //Motor direction enumeration selection
   export enum MotorDir {
@@ -25,19 +23,49 @@ namespace bb {
     Backward,
   }
 
-  //% block="go forward for|%duration ms"
-  export function goForward(
-    duration: number = 1000,
+  //% block="go forward|%dist inches"
+  //% dist.min=0 dist.defl=12
+  export function goForward(dist: number, speed: number = 100): void {
+    let duration = internal.inchesToMsAtSpeed(dist, speed);
+    let dir = MotorDir.Forward;
+    internal.setMotors(dir, speed, dir, speed);
+    basic.pause(duration);
+    internal.setMotors(dir, 0, dir, 0);
+  }
+
+  //% block="go backward|%dist inches"
+  //% dist.min=0 dist.defl=12
+  export function goBackward(
+    dist: number,
     speed: number = 100
   ): void {
-    internal.setMotors(
-      MotorDir.Forward,
-      speed,
-      MotorDir.Forward,
-      speed
-    );
+    let duration = internal.inchesToMsAtSpeed(dist, speed);
+    let dir = MotorDir.Backward;
+    internal.setMotors(dir, speed, dir, speed);
     basic.pause(duration);
-    internal.setMotors(MotorDir.Forward, 0, MotorDir.Forward, 0);
+    internal.setMotors(dir, 0, dir, 0);
+  }
+
+  //% block="turn left|%deg degrees"
+  //% deg.min=0 deg.defl=90
+  export function turnLeft(deg: number, speed: number = 100): void {
+    let duration = internal.degreesToMsAtSpeed(deg, speed);
+    let leftDir = MotorDir.Backward;
+    let rightDir = MotorDir.Forward;
+    internal.setMotors(leftDir, speed, rightDir, speed);
+    basic.pause(duration);
+    internal.setMotors(leftDir, 0, rightDir, 0);
+  }
+
+  //% block="turn right|%deg degrees"
+  //% deg.min=0 deg.defl=90
+  export function turnRight(deg: number, speed: number = 100): void {
+    let duration = internal.degreesToMsAtSpeed(deg, speed);
+    let leftDir = MotorDir.Forward;
+    let rightDir = MotorDir.Backward;
+    internal.setMotors(leftDir, speed, rightDir, speed);
+    basic.pause(duration);
+    internal.setMotors(leftDir, 0, rightDir, 0);
   }
 
   namespace internal {
@@ -48,12 +76,28 @@ namespace bb {
       rightSpeed: number
     ): void {
       let allBuffer = pins.createBuffer(5);
-      allBuffer[0] = LEFT_MOTOR_REGISTER;
+      allBuffer[0] = MOTOR_SPEEDS_REGISTER;
       allBuffer[1] = leftDir;
       allBuffer[2] = leftSpeed;
       allBuffer[3] = rightDir;
       allBuffer[4] = rightSpeed;
       pins.i2cWriteBuffer(I2CADDR, allBuffer);
+    }
+
+    export function inchesToMsAtSpeed(
+      inches: number,
+      speed: number
+    ): number {
+      let inches_per_ms = speed * SPEED_TO_IPMS;
+      return inches / inches_per_ms;
+    }
+
+    export function degreesToMsAtSpeed(
+      degrees: number,
+      speed: number
+    ): number {
+      let degrees_per_ms = speed * SPEED_TO_DPMS;
+      return degrees / degrees_per_ms;
     }
   }
 }
