4983b0932427bf4d85193d792c00ab59a018ae81
Michael Schaffer <ichaelm1@gmail.com>
2023-09-20 11:34:59 -0400

    Test block name

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  */
 //% color=230 weight=50 icon="\uf067" block="Brainybot"
 namespace bb {
-  //% block
+  //% block="Show Number"
   export function showNumber(
     v: number,
     interval: number = 150
