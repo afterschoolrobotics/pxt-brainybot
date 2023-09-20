48ca7fc266bf30d6faf0d2fcfae35d896db8f426
Michael Schaffer <ichaelm1@gmail.com>
2023-09-20 11:10:55 -0400

    Testing first changes to default files

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
-})
+/**
+ * Control an ASR Brainybot robot.
+ */
+//% color=230 weight=100 icon="\uf544" block="Brainybot
+namespace bb {
+  //% block
+  export function showNumber(
+    v: number,
+    interval: number = 150
+  ): void {}
+}
+
+// basic.forever(function () {
+//
+// })
