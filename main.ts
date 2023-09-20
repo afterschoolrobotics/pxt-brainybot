08daaff4d865903aee046901df0da8207faceaf2
Michael Schaffer <ichaelm1@gmail.com>
2023-09-20 11:41:29 -0400

    Test drive block naming

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  */
 //% color=230 weight=50 icon="\uf067" block="Brainybot"
 namespace bb {
-  //% block="Show|Number %v"
+  //% block="drive forwards|%v seconds"
   export function showNumber(
     v: number,
     interval: number = 150
